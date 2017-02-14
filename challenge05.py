c32e2f11835437d6daf8d44e9bba2e10cdd203ed
Rikkokiri <pilvi.rajala@gmail.com>
2017-02-14 15:55:33 +0200

    Level 5 solved

---
+++ challenge05.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Python challenge
+#### Challenge 5 ######
+
+# http://www.pythonchallenge.com/pc/def/peak.html
+
+from __future__ import print_function
+import pickle
+
+
+# Open the file for reading
+f = open('pickled.p', 'r')
+
+# load the object from the file into var b
+matrix = pickle.load(f)
+f.close()
+
+for row in matrix:
+    #print row
+    rowstring = ""
+
+    for cell in row:
+        print(cell[0]*cell[1], end='')
+    print()
